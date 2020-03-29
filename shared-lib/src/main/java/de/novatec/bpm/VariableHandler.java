7ec0d5f5f6b50e4c383edf2b71458a325539e8ea
Stefan Schultz <ssh@novatec-gmbh.de>
2020-03-29 12:02:46 +0200

    Added setter for process variabels

--- shared-lib/src/main/java/de/novatec/bpm/VariableHandler.java
+++ shared-lib/src/main/java/de/novatec/bpm/VariableHandler.java
@@ -6,10 +6,18 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 public class VariableHandler {
 
     public static String get(DelegateExecution execution, ProcessVariable variable) {
-        if(execution.getVariables().containsKey(variable.getName())) {
+        if (execution.getVariables().containsKey(variable.getName())) {
             return (String) execution.getVariable(variable.getName());
         } else {
             return null;
+        }
+    }
+
+    public static void set(DelegateExecution execution, ProcessVariable variable, String value) {
+        if (variable != null) {
+            execution.setVariable(variable.getName(), value);
+        } else {
+            throw new IllegalArgumentException("The variable to set cannot be null");
         }
     }
 
